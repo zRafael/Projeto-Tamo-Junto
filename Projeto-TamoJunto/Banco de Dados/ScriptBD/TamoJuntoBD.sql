b6d26755b8af7e5ce29df735d26d5e5a7e37f39b
Rafael de Campos <79462714+zRafael@users.noreply.github.com>
2021-05-24 19:37:57 -0300

    Add files via upload

---
+++ Projeto-TamoJunto/Banco de Dados/ScriptBD/TamoJuntoBD.sql
@@ -0,0 +1,74 @@
+-- Criação do banco de dados do TamoJuntoCifra
+create database TamoJuntoCifra;
+-- Usando o TamoJuntoCifra Banco de Dados
+use TamoJuntoCifra;
+
+-- Criando Tabela Usuario
+create table usuario (
+idUsuario int primary key auto_increment,
+nomeUsuario varchar(45),
+dataNasc date,
+cpfUsuario char(14),
+emailUsuario varchar(45),
+sexo char (1) check (sexo ='m' or sexo='f' or sexo='n')
+);
+
+-- Criando Tabela telefone
+create table telefone (
+idTelefone int primary key auto_increment,
+telefoneCel char(14),
+fkUsuario int,
+foreign key (fkUsuario) references usuario (idUsuario)
+);
+
+-- Criando Tabela Cifras Musica
+create table cifrasMusica (
+idCifras int primary key auto_increment,
+nomeMusica varchar(45),
+nomeBanda varchar(45),
+gravadora varchar (45),
+tomMusica char(4),
+fkUsuario int,
+foreign key (fkUsuario) references usuario (idUsuario)
+);
+
+-- Criando Tabela Estilo Musical
+create table estiloMusical (
+idEstilo int primary key,
+nomeEstilo varchar(45)
+);
+
+-- Criando tabela Associativa entre Estilo e Usuario
+create table EstiloUsuario (
+fkEstilo int,
+foreign key (fkEstilo) references estiloMusical (idEstilo),
+fkUsuario int,
+foreign key (fkUsuario) references usuario (idUsuario),
+primary key (fkEstilo, fkUsuario)
+);
+
+-- Criando Tabela aulas 
+create table aulas (
+idAulas int primary key auto_increment,
+nomeProfessor varchar(45),
+tipoInstrumento varchar(30)
+);
+
+-- Criando Tabela Associativa entre Usuario e Aulas
+create table usuarioAulas (
+fkUsuario int,
+foreign key (fkUsuario) references usuario (idUsuario),
+fkAulas int,
+foreign key (fkAulas) references aulas (idAulas),
+primary key (fkUsuario, fkAulas)
+);
+
+
+
+
+
+
+
+
+
+
